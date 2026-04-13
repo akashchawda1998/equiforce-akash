b1d67a8bee05f551fa7d07e061a1f38e854cfbdf
Akash Chawda <akashchawda1998@gmail.com>
2026-04-13 09:28:03 +0530

    Update Hero.jsx

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -23,9 +23,9 @@ function Hero() {
             Nearly 3 Decades of Industry Expertise
           </p>
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold mb-4 sm:mb-6 leading-snug sm:leading-tight  lg:text-left">
-            Your Strategic <span className="text-[#d97706]">Partner</span> in
+            Your Strategic <span className="text-[#d97706]"> Partner </span>in
             <br className="hidden sm:block" />
-            Investment Ops, Performance & GIPS
+             <span> Investment Ops, Performance & GIPS</span>
           </h1>
           <p className="uppercase max-w-lg text-gray-300 mb-8 font-medium text-sm md:text-base">
             <span className="text-[#d97706]">
