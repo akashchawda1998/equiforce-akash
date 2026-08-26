3030e748cf1a43898892990f36484ab442a6a36b
Akash Chawda <akashchawda1998@gmail.com>
2026-08-27 00:17:51 +0530

    Update AboutHero.jsx

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -4,36 +4,39 @@ import aboutImg from "../../assets/images/about-image1.webp";
 const AboutHero = () => {
   return (
 
-<>
+    <>
 
-<title>About EquiForce | Performance Measurement & GIPS® Solutions and AI-powered reporting</title>
-            <Helmet>
+      <title>About EquiForce | Performance Measurement & GIPS® Solutions and AI-powered reporting</title>
+      <Helmet>
 
-<meta name="description" content="EquiForce delivers specialized solutions across performance measurement, GIPS® composite management and AI-Powered Investment Reporting.">
+        <meta name="description" content="EquiForce delivers specialized solutions across performance measurement, GIPS® composite management and AI-Powered Investment Reporting.">
 
-</meta>
-</Helmet>
-    <section className="pt-22 px-5 md:pt-30 lg:pt-30 pb-12 md:pb-10 lg:pb-10 text-center">
-      <h1 className="text-3xl md:text-5xl font-bold text-gray-800 mb-10">
-        Our Mission
-      </h1>
+        </meta>
+      </Helmet>
+      <section className="pt-22 px-5 md:pt-30 lg:pt-30 pb-12 md:pb-10 lg:pb-10 text-center">
+        <h1 className="text-3xl md:text-5xl font-bold text-gray-800 mb-10">
+          Our Mission
+        </h1>
 
-      <p className="text-gray-600 max-w-4xl mx-auto font-medium">
-        EquiForce empowers investment management firms with{" "}
-        <span className="text-[#FF8000]"> purpose-built </span> technology -
-        delivering precision-engineered solutions for performance measurement, GIPS® composite
-        management, and AI-powered investment reporting. More than a software
-        provider, we are a{" "}
-        <span className="text-[#FF8000]">
-          strategic partner and consultants
-        </span>{" "}
-        with nearly three decades of investment operations expertise -
-        integrating <span className="text-[#FF8000]">AI technologies</span> that
-        enable our clients to operate with accuracy, confidence and a
-        competitive edge.
-      </p>
-    </section>
-</>
+        <p className="text-gray-600 max-w-4xl mx-auto font-medium">
+
+
+
+          EquiForce empowers investment management firms with{" "}
+          <span className="text-[#FF8000]"> purpose-built </span> technology -
+          delivering precision-engineered solutions for performance measurement, GIPS® composite
+          management, and AI-powered investment reporting. More than a software
+          provider, we are a{" "}
+          <span className="text-[#FF8000]">
+            strategic partner and consultants
+          </span>{" "}
+          with over three decades of investment operations expertise -
+          integrating <span className="text-[#FF8000]">AI technologies</span> that
+          enable our clients to operate with accuracy, confidence and a
+          competitive edge.
+        </p>
+      </section>
+    </>
 
   );
 };
